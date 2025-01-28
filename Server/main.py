9491c6a10a97408daee62279c2e760e35ba92f41
VivekBhand <bhandvv19.comp@coep.ac.in>
2025-01-29 00:36:13 +0530

    Another way to pass parameter via request body

--- Server/main.py
+++ Server/main.py
@@ -1,10 +1,16 @@
 from fastapi import FastAPI, Request
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Test(BaseModel):
+    name: str
+    age : int
+    
 
 @app.post('/')
 # Any function parameter passed directly is considered as a query param ?
 # To use request body we have import and use request from fastapi
-async def test(request : Request):
-    print((await request.body()).decode())
+def test(t: Test):
+    print(t)
     return 'Hello World'
